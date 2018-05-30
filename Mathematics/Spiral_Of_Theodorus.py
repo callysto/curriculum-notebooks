67cc5555d91628f1f884e8aea4c17e92e0c810d1
jchalatu <37821141+jchalatu@users.noreply.github.com>
2018-05-30 10:05:28 -0600

    Working version of CC-49 w/ gif file
    -Added gif
    -Modified Spiral of Theodorus
    -Added extra example  questions at end of notebook

--- Mathematics/Spiral_Of_Theodorus.py
+++ Mathematics/Spiral_Of_Theodorus.py
@@ -10,13 +10,15 @@ import matplotlib.pyplot as plt
 from math import atan, sqrt, cos, sin
 col = ['#e02727','#e06e27','#e0ca27','#a9e027','#57e027','#27e07e','#27e0bc','#27cde0','#2789e0','#2750e0','#2c27e0','#6827e0','#8427e0','#b527e0','#e027de','#e0279f','#e02766']
 c = len(col)
+
 def Spiral(n):
-    col_counter = 0 
+    col_counter = 0
     N = np.arange(1,n)
     phi = 0
-    plt.figure(1,figsize = (10,7))
+    plt.figure(1,figsize = (12,9))
     hold = True
     for n in N:
+
         phi_n = atan(1./sqrt(n))
         phi += phi_n
         if n == 1:
@@ -30,7 +32,7 @@ def Spiral(n):
             plt.text(x/1.4,y/1.4,string)
             X = [0,1,x]
             Y = [0,0,y]
-            plt.fill(X,Y,color = col[col_counter],alpha = 0.3)            
+            plt.fill(X,Y,color = col[col_counter],alpha = 0.45)            
             last_x = 1
             last_y = 1
         else:
@@ -40,14 +42,14 @@ def Spiral(n):
             plt.plot([0,x], [0,y], linewidth = 1.5,color = col[col_counter])
             plt.plot([last_x,x], [last_y,y],color = col[col_counter])
             string = str((r'$\sqrt{' + str(n + 1) + '}$'))
-            plt.text(x/1.4,y/1.4,string)
+            plt.text(x/1.4,y/1.4,string,fontsize = 13)
             X = [0,last_x,x]
             Y = [0,last_y,y]
-            plt.fill(X,Y,color = col[col_counter],alpha = 0.3)
+            plt.fill(X,Y,color = col[col_counter],alpha = 0.45)
             last_x = x
             last_y = y
 
-            
         col_counter += 1
         if col_counter > c-1:
             col_counter = 0
+
