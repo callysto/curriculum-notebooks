b00c8585835d2fbdec6bd7a8e5c72b36ba431264
Nicole Vachon <n.vachon@uleth.ca>
2018-12-14 22:41:40 +0000

    Fixed small issues, still need to fix drag and drop

--- Humanities/IntroductionToPoetry/sources/syllables.js
+++ Humanities/IntroductionToPoetry/sources/syllables.js
@@ -1,34 +1,34 @@
 function display(question) {
 	var data1 = [{
-		original: "There", hyphen: "There", xvar: 0, move: 0
+		original: "There", hyphen: "There", xvar: 0
 		},{		
-		original: "is", hyphen: "is", xvar: 55, move: 0
+		original: "is", hyphen: "is", xvar: 55
 		},{
-		original: "an", hyphen: "an", xvar: 73, move: 0
+		original: "an", hyphen: "an", xvar: 73
 		},{		
-		original: "elephant", hyphen: "e-le-phant", xvar: 98, move: 3
+		original: "elephant", hyphen: "e-le-phant", xvar: 98
 		},{		
-		original: "in", hyphen: "in", xvar: 180, move: 0
-		},{	
-		original: "my", hyphen: "my", xvar: 200, move: 0
+		original: "in", hyphen: "in", xvar: 180
+		},{
+		original: "my", hyphen: "my", xvar: 200
 		},{		
-		original: "closet.", hyphen: "clo-set.", xvar: 230, move: 0
+		original: "closet.", hyphen: "clo-set.", xvar: 230
 		}];
 		
 	var data2 = [{
-		original: "All", hyphen: "All", xvar: 0, move: 0
+		original: "All", hyphen: "All", xvar: 0
 		},{		
-		original: "hippos", hyphen: "hip-pos", xvar: 27, move: 3
+		original: "hippos", hyphen: "hip-pos", xvar: 27
 		},{
-		original: "go", hyphen: "go", xvar: 90, move: 0
+		original: "go", hyphen: "go", xvar: 90
 		},{		
-		original: "swimming", hyphen: "swim-ming", xvar: 115, move: 2
+		original: "swimming", hyphen: "swim-ming", xvar: 115
 		},{		
-		original: "in", hyphen: "in", xvar: 206, move: 0
+		original: "in", hyphen: "in", xvar: 206
 		},{	
-		original: "the", hyphen: "the", xvar: 226, move: 0
+		original: "the", hyphen: "the", xvar: 226
 		},{		
-		original: "summer.", hyphen: "sum-mer.", xvar: 258, move: 0
+		original: "summer.", hyphen: "sum-mer.", xvar: 258
 		}];
 		
 	if (question == 1) {
@@ -66,8 +66,8 @@ function display(question) {
 	
 	function handleMouseOver(d, i) {
 		d3.select(this).text(function(d) {return d.hyphen})
-			.attr("x", function (d,i) { return d.xvar - d.move })
-			.attr("font-size", "18px")
+			//.attr("x", function (d,i) { return d.xvar })
+			.attr("font-size", "17px")
 			.attr("fill", "red");
 			//.attr("style", "backgroundColor: powderblue");
 	}
