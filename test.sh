bef4e8bb8779f32fe7cb4a0eb51090fba41c8d9e
Cameron Mann <cameron.mann@cybera.ca>
2019-02-01 13:46:50 +0700

    Add checks for whitespace and checkpoint dirs

--- test.sh
+++ test.sh
@@ -20,4 +20,20 @@ for notebook in "${notebooks[@]}"; do
     [ $? -ne 0 ] && retval=1
 done
 
+checkpoint_dirs=($(git ls-files *.ipynb_checkpoints/*))
+
+if [ ${#checkpoint_dirs[@]} -ne 0 ]; then
+    echo -e "\n===== These .ipynb_checkpoints directories should not be committed and need to be removed ====="
+    echo "${checkpoint_dirs[*]}"
+    retval=1
+fi
+
+bad_paths=($(git ls-files *.ipynb | grep " "))
+
+if [ ${#bad_paths[@]} -ne 0 ]; then
+    echo -e "\n===== Whitespace is not allowed in notebook paths, these files need to be renamed ====="
+    echo "${bad_paths[*]}"
+    retval=1
+fi
+
 exit $retval
