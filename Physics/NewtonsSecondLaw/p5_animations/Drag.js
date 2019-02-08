afa8f9cd2225444600a969b6cb8882404f708c6c
Cameron Mann <cameron.mann@cybera.ca>
2019-02-08 17:24:14 +0700

    Make physics anims repeatable

--- Physics/NewtonsSecondLaw/p5_animations/Drag.js
+++ Physics/NewtonsSecondLaw/p5_animations/Drag.js
@@ -1,42 +1,39 @@
-var b = [];
-var M;
-var R;
+var balls = [];
 var liquid;
 var bub1 = [];
 var bub2 = [];
+var state = 0;
 
 function setup() {
-  createCanvas(750,600);
-  var r_old = 0;
-  liquid = new Friction(0,height/1.8,width,height/2,0.1);
+  createCanvas(750, 600);
+  reset();
+  liquid = new Friction(0, height/1.8, width, height/2, 0.1);
+}
+
+function reset() {
+  var radius_old = 0;
+  state = 0;
+
   for (var i = 0; i < 40; i++) {
-    M = random(1,11);
-    R = M*5;
-    if (i == 0)
-    {
-    	b[i] = new Ball(M,R/2,100,R);
+    var mass = random(1, 11);
+    var radius = mass*5;
+
+    if (i == 0) {
+    	balls[i] = new Ball(mass, radius/2, 100, radius);
+    } else {
+    	balls[i] = new Ball(mass, radius/2+radius_old, 100, radius);
     }
-    else 
-    {
-    	b[i] = new Ball(M,R/2+r_old,100,R);
-    }
-    r_old += R;
+
+    radius_old += radius;
   }
+
   for (var i = 0; i < 50; i++) {
     bub1[i] = new Bubbles();
   }
   for (var i = 0; i < 50; i++) {
     bub2[i] = new Bubbles();
   }
-};
-
-// function drawArrow(x0,y0,x1,y1) {
-//   strokeWeight(20);
-//   stroke(255, 87, 51);
-//   triangle(x1,y1+2,x1,y1-2,x1+3.5,y1);
-//   line(x0,y0,x1,y1);
-// };
-var check = 0;
+}
 
 function draw() {
   background(255);
@@ -44,15 +41,12 @@ function draw() {
   stroke(0);
   fill(0);
 
-  if (mouseIsPressed) {
-    check = 1;
-  }
-
-  if (check == 0) {
-    var txt = new r_text("CLICK TO APPLY GRAVITATIONAL FORCE");
-  }
-  else {
-    var txt = new r_text(" ");
+  if (state == 0) {
+    new r_text("CLICK TO APPLY GRAVITATIONAL FORCE");
+  } else if (state == 1) {
+    new r_text("");
+  } else {
+    new r_text("CLICK TO RESET")
   }
 
   for (var i = 0; i < bub1.length; i++) {
@@ -61,22 +55,22 @@ function draw() {
     bub1[i].floater(0.07);
   }
 
-  for (var i = 0; i < b.length; i++) {
-    var gravity = createVector(0,0.1*b[i].mass);
+  for (var i = 0; i < balls.length; i++) {
+    var gravity = createVector(0, 0.1*balls[i].mass);
 
-    if (check == 1) {
-      b[i].applyForce(gravity);
+    if (state != 0) {
+      balls[i].applyForce(gravity);
     }
 
-    b[i].isInside(liquid);
+    balls[i].isInside(liquid);
 
-    if (b[i].edges == 1) {
-      b[i].Drag(liquid);
+    if (balls[i].edges == 1) {
+      balls[i].Drag(liquid);
     }
 
-    b[i].display();
-    b[i].update();
-    b[i].checkEdges();
+    balls[i].display();
+    balls[i].update();
+    balls[i].checkEdges();
   }
 
   for (var i = 0; i < bub2.length; i++) {
@@ -86,26 +80,33 @@ function draw() {
   }
 };
 
-
+function mousePressed() {
+  if (state == 0) {
+    state = 1;
+  } else if (state == 2) {
+    reset();
+  } 
+}
 
 function r_text(str) {
-	text(str,255,240);
+	text(str, 255, 240);
 }
 
 function Bubbles() {
-  this.location = createVector(random(0,width),random(height/1.8,height));
+  this.location = createVector(random(0, width), random(height/1.8, height));
+};
 
 Bubbles.prototype.display = function() {
-  stroke( 105, 145, 207);
+  stroke(105, 145, 207);
   strokeWeight(0.7);
-  fill(105, 145, 207,0.8);
-  ellipse(this.location.x,this.location.y,5,5);
+  fill(105, 145, 207, 0.8);
+  ellipse(this.location.x, this.location.y, 5, 5);
 };
 
 Bubbles.prototype.floater = function(flt_rate) {
   var B_x = this.location.x;
   var B_y = this.location.y;
-  var mov = random(-1,1)*0.1
+  var mov = random(-1, 1)*0.1
   this.location.x += mov;
   this.location.y -= flt_rate;
 };
@@ -113,37 +114,36 @@ Bubbles.prototype.floater = function(flt_rate) {
 Bubbles.prototype.clr = function() {
   if (this.location.y < height/1.78) {
     this.location.y = height;
-    this.location.x = random(0,width);
+    this.location.x = random(0, width);
   }
 };
-};
 
-function Friction(X,Y,W,H,cd) {
+function Friction(X, Y, W, H, cd) {
   this.C = cd;
   this.x = X;
   this.y = Y;
   this.w = W;
   this.h = H;
+};
 
 Friction.prototype.display = function() {
   noStroke();
   fill(35, 98, 195);
-  rect(this.x,this.y,this.w,this.h);
+  rect(this.x, this.y, this.w, this.h);
 };
 
-};
-
-function Ball(m,start_x,start_y,R) {
+function Ball(m, start_x, start_y, R) {
   this.location = createVector(start_x, start_y);
-  this.velocity = createVector(0,0);
-  this.acceleration = createVector(0,0);
+  this.velocity = createVector(0, 0);
+  this.acceleration = createVector(0, 0);
   this.mass = m;
   this.rad = R;
   this.edges = 0;
-  this.drag = createVector(0,0);
+  this.drag = createVector(0, 0);
+};
 
 Ball.prototype.applyForce = function(f) {
-  var grav = p5.Vector.div(f,this.mass);
+  var grav = p5.Vector.div(f, this.mass);
   this.acceleration.add(grav);
 };
 
@@ -162,42 +162,29 @@ Ball.prototype.isInside = function(F) {
 Ball.prototype.Drag = function(F) {
   var speed = this.velocity.mag();
   var forceMag = F.C * speed * speed;
-  var force = createVector(this.velocity.x,this.velocity.y);
+  var force = createVector(this.velocity.x, this.velocity.y);
   force.mult(-1);
   force.normalize();
   force.mult(forceMag);
-  var drag = p5.Vector.div(force,this.mass);
+  var drag = p5.Vector.div(force, this.mass);
   this.acceleration.add(drag);
-
 };
 
 Ball.prototype.display = function() {
   stroke(0);
   strokeWeight(1);
-  fill(199,0,57);
-  ellipse(this.location.x,this.location.y,this.rad,this.rad);
+  fill(199, 0, 57);
+  ellipse(this.location.x, this.location.y, this.rad, this.rad);
 };
 
-
 Ball.prototype.checkEdges = function() {
-  // if (this.location.x > width) {
-  //   this.location.x = width;
-  //   this.velocity.x *= -1;
-
-  // } else if (this.location.x < 0) {
-  //   this.velocity.x *= -1;
-  //   this.location.x = 0;
-  // }
-  
   if (this.location.y > height) {
     this.velocity.y *= 1;
     this.location.y = height;
+    state = 2;
   }
   if (this.location.y < 0) {
     this.velocity.y *= -1;
     this.location.y = 0;
   }
 };
-};
-
-
--- Physics/NewtonsSecondLaw/p5_animations/Gravity.js
+++ Physics/NewtonsSecondLaw/p5_animations/Gravity.js
@@ -1,37 +1,39 @@
-var b = [];
-var M;
-var R;
+var balls = [];
 var star = [];
+var state = 0;
 
 function setup() {
-  createCanvas(600,400);
-  var r_old = 0;
-  for (var i = 0; i < 20; i++) {
-    M = random(1,6);
-    R = M*15;
-    if (i == 0)
-    {
-    	b[i] = new Ball(M,R/2,100,R);
-    }
-    else 
-    {
-    	b[i] = new Ball(M,R/2+r_old,100,R);
-    }
-    r_old += R;
-  }
+  createCanvas(600, 400);
+  reset();
+
   for (var i = 0; i < 500; i++) {
     var x = random(width);
     var y = random(height);
-    star[i] = new Star(0.8,x,y);
+    star[i] = new Star(0.8, x, y);
   }
+}
 
-};
+function reset() {
+  var radius_old = 0;
+  state = 0;
+  
+  for (var i = 0; i < 20; i++) {
+    var mass = random(1, 6);
+    var radius = mass*15;
 
-var check = 0;
+    if (i == 0) {
+    	balls[i] = new Ball(mass, radius/2, 100, radius);
+    } else {
+    	balls[i] = new Ball(mass, radius/2+radius_old, 100, radius);
+    }
+
+    radius_old += radius;
+  }
+}
 
 function draw() {
   background(0);
-  var gravity = createVector(0,0.1);
+  var gravity = createVector(0, 0.1);
 
   for (var i = 0; i < star.length; i++) {
     star[i].display();
@@ -40,45 +42,48 @@ function draw() {
   stroke(255);
   fill(255);
 
-  if (check == 0) {
-    var txt = new r_text("CLICK TO APPLY GRAVITATIONAL FORCE");
-  }
-  else {
-    var txt = new r_text("");
-  }
-
-  if (mouseIsPressed) {
-    check = 1;
+  if (state == 0) {
+    new r_text("CLICK TO APPLY GRAVITATIONAL FORCE");
+  } else if (state == 1) {
+    new r_text("");
+  } else {
+    new r_text("CLICK TO RESET")
   }
 
-  for (var i = 0; i < b.length; i++) {
-    if (check == 1) {
-      b[i].applyForce(gravity);
+  for (var i = 0; i < balls.length; i++) {
+    if (state == 1) {
+      balls[i].applyForce(gravity);
     }
-    b[i].display();
-    b[i].update();
-    b[i].checkEdges();
+    balls[i].display();
+    balls[i].update();
+    balls[i].stateEdges();
   }
-
 };
 
-
-function r_text(str) {
-	text(str,200,250);
+function mousePressed() {
+  if (state == 0) {
+    state = 1;
+  } else if (state == 2) {
+    reset();
+  } 
 }
 
-function Ball(m,start_x,start_y,R) {
+function r_text(str) {
+	text(str, 200, 250);
+}
+
+function Ball(mass, start_x, start_y, radius) {
   this.location = createVector(start_x, start_y);
-  this.velocity = createVector(0,0);
-  this.acceleration = createVector(0,0);
-  this.mass = m;
-  this.rad = R;
+  this.velocity = createVector(0, 0);
+  this.acceleration = createVector(0, 0);
+  this.mass = mass;
+  this.rad = radius;
+};
 
 Ball.prototype.applyForce = function(force) {
-  var f = force;
-  this.acceleration.add(f);
+  this.acceleration.add(force);
 };
-  
+
 Ball.prototype.update = function() {
   this.velocity.add(this.acceleration);
   this.location.add(this.velocity);
@@ -88,34 +93,30 @@ Ball.prototype.update = function() {
 Ball.prototype.display = function() {
   stroke(0);
   strokeWeight(1);
-  fill(199,0,57);
-  ellipse(this.location.x,this.location.y,this.rad,this.rad);
+  fill(199, 0, 57);
+  ellipse(this.location.x, this.location.y, this.rad, this.rad);
 };
 
-
-Ball.prototype.checkEdges = function() {
+Ball.prototype.stateEdges = function() {
   if (this.location.y > height) {
     this.velocity.y *= 1;
     this.location.y = height;
+    state = 2;
   }
   if (this.location.y < 0) {
     this.velocity.y *= -1;
     this.location.y = 0;
   }
 };
+
+function Star(radius, x, y) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
 };
 
-
-
-function Star(r, X, Y) {
-  this.x = X;
-  this.y = Y;
-  this.radius = r;
- 
 Star.prototype.display = function() {
   noStroke();
-  fill(255,255,255);
-  ellipse(this.x,this.y,this.radius,this.radius);
-};
-
+  fill(255, 255, 255);
+  ellipse(this.x, this.y, this.radius, this.radius);
 };
